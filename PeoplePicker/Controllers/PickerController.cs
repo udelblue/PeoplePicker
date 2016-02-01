c3a6266c7b8a991c60caae3d21417643f73b6785
udelblue <cdsommers@gmail.com>
2016-02-01 18:43:50 -0500

    api modified

--- PeoplePicker/Controllers/PickerController.cs
+++ PeoplePicker/Controllers/PickerController.cs
@@ -1,32 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.DirectoryServices;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using PeoplePicker.Models;
+using PeoplePicker.Util;
 
 namespace PeoplePicker.Controllers
 {
     public class PickerController : ApiController
     {
-        Person[] people = new Person[] 
-        { 
-            new Person { Id = 1, Name = "John Smith"}, 
-            new Person { Id = 2, Name = "Tommy Tester" }, 
-            new Person { Id = 3, Name = "Tina Tester" }, 
-            new Person { Id = 4, Name = "Jon Jones" },
-            new Person { Id = 5, Name = "Jimmy Jones" },
-            new Person { Id = 6, Name = "Ted Tester" }
-        };
+        //Person[] people = new Person[] 
+        //{ 
+        //    new Person { Id = 1, Name = "John Smith"}, 
+        //    new Person { Id = 2, Name = "Tommy Tester" }, 
+        //    new Person { Id = 3, Name = "Tina Tester" }, 
+        //    new Person { Id = 4, Name = "Jon Jones" },
+        //    new Person { Id = 5, Name = "Jimmy Jones" },
+        //    new Person { Id = 6, Name = "Ted Tester" }
+        //};
 
      
 
         public IEnumerable<Person> GetPeople(string name)
         {
-            var productslist = people.Where(p => p.Name.ToLower().StartsWith(name.ToLower()));
+            string domain = "google.com";
+            List<Person> peoplelist = new List<Person>();
+             List<DirectoryEntry> del = ADHelper.GetPeople(domain , name);
+             if (del.Count >= 1)
+             {
+                 foreach (var de in del)
+                 {
+                     Person p = new Person();
+                     p.Name = de.Properties["givenName"].Value + " " + de.Properties["sn"].Value;
+                     peoplelist.Add(p);
+                 }
+             }
 
-            return productslist;
+           // var productslist = people.Where(p => p.Name.ToLower().StartsWith(name.ToLower()));
+
+            return peoplelist;
         }
 
     }
